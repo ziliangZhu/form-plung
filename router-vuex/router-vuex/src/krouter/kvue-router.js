5c70e2b6992a3c67b7954618a6ed3cd3412e8c19
zzl <1591770959@qq.com>
2020-08-10 22:56:37 +0800

    add component

--- router-vuex/router-vuex/src/krouter/kvue-router.js
+++ router-vuex/router-vuex/src/krouter/kvue-router.js
@@ -4,6 +4,21 @@ let Vue
 class KVueRouter {
   constructor (options) {
     this.$options = options
+    // 需要创建响应式的current
+    Vue.util.defineReactive(this, 'current', '/')
+    // this.current = '/'
+    // 监听URL变化
+    window.addEventListener('hashchange', this.onHashChange.bind(this))
+    window.addEventListener('load', this.onHashChange.bind(this))
+
+    this.routeMap = {}
+    options.routes.forEach(route => {
+      this.routeMap[route.path] = route
+    })
+  }
+
+  onHashChange () {
+    this.current = window.location.hash.slice(1)
   }
 }
 
@@ -40,7 +55,20 @@ KVueRouter.install = function (_Vue) {
       }, this.$slots.default)
     }
   })
-  Vue.component('router-view', {})
+  Vue.component('router-view', {
+    render (h) {
+    // 获取path对应的component
+      /* let component = null
+      this.$router.$options.routes.forEach(route => {
+        if (route.path === this.$router.current) {
+          component = route.component
+        }
+      }) */
+      const { routeMap, current } = this.$router
+      const component = routeMap[current].component || null
+      return h(component)
+    }
+  })
 }
 
 export default KVueRouter
